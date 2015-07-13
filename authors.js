f90f0c73363a2e6f3767b8954404ffc266d620d6
Vladimir Starkov <vladimir.starkov@nordnet.se>
2015-07-13 13:33:58 +0200

    add @artemeff

--- authors.js
+++ authors.js
@@ -1,5 +1,6 @@
 export default [
   // post: false
+  { username: 'artemeff',      start: '13 jule 2015', first: '620478074027679744', post: false },
   { username: 'suxxes',        start: '06 jule 2015', first: '617971487052591105' },
   { username: 'anton_davydov', start: '29 june 2015', first: '615408833276551168' },
 ];
