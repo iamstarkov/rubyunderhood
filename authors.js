cd42525005110c5074a80b3a2ec12598e36e462c
Vladimir Starkov <iamstarkov@users.noreply.github.com>
2015-07-23 14:14:27 +0200

    Update authors.js

--- authors.js
+++ authors.js
@@ -1,7 +1,7 @@
 export default [
   // post: false
-  { username: 'rainrb',        start: '20 jule 2015', first: '623035043632558080', post: false },
-  { username: 'artemeff',      start: '13 jule 2015', first: '620478074027679744' },
-  { username: 'suxxes',        start: '06 jule 2015', first: '617971487052591105' },
+  { username: 'rainrb',        start: '20 july 2015', first: '623035043632558080', post: false },
+  { username: 'artemeff',      start: '13 july 2015', first: '620478074027679744' },
+  { username: 'suxxes',        start: '06 july 2015', first: '617971487052591105' },
   { username: 'anton_davydov', start: '29 june 2015', first: '615408833276551168' },
 ];
