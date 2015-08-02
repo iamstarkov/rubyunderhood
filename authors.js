f8be6d644af909cf7e55012cb5d07bcdbd49156e
Vladimir Starkov <iamstarkov@users.noreply.github.com>
2015-08-03 01:26:13 +0200

    publish @g3d

--- authors.js
+++ authors.js
@@ -1,6 +1,6 @@
 export default [
   // post: false
-  { username: 'g3d',           start: '27 july 2015', first: '625520018697134080', post: false },
+  { username: 'g3d',           start: '27 july 2015', first: '625520018697134080' },
   { username: 'rainrb',        start: '20 july 2015', first: '623035043632558080' },
   { username: 'artemeff',      start: '13 july 2015', first: '620478074027679744' },
   { username: 'suxxes',        start: '06 july 2015', first: '617971487052591105' },
