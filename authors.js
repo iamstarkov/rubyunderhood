533a64e241b5e07327f1c168d891c7630af91a98
Vladimir Starkov <iamstarkov@users.noreply.github.com>
2015-08-03 08:52:33 +0200

    add @vazilla

--- authors.js
+++ authors.js
@@ -1,8 +1,9 @@
 export default [
   // post: false
-  { username: 'g3d',           start: '27 july 2015', first: '625520018697134080' },
-  { username: 'rainrb',        start: '20 july 2015', first: '623035043632558080' },
-  { username: 'artemeff',      start: '13 july 2015', first: '620478074027679744' },
-  { username: 'suxxes',        start: '06 july 2015', first: '617971487052591105' },
-  { username: 'anton_davydov', start: '29 june 2015', first: '615408833276551168' },
+  { username: 'vazilla',       start: '03 aug 2015', first: '628080874077200384', post: false },
+  { username: 'g3d',           start: '27 jul 2015', first: '625520018697134080' },
+  { username: 'rainrb',        start: '20 jul 2015', first: '623035043632558080' },
+  { username: 'artemeff',      start: '13 jul 2015', first: '620478074027679744' },
+  { username: 'suxxes',        start: '06 jul 2015', first: '617971487052591105' },
+  { username: 'anton_davydov', start: '29 jun 2015', first: '615408833276551168' },
 ];
