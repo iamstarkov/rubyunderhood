630a53d99630717b8bec3698ed6fe55444cde11d
Anton Davydov <don.mist@gmail.com>
2015-11-01 22:40:56 +0300

    Update authors.js

--- authors.js
+++ authors.js
@@ -1,6 +1,6 @@
 export default [
   // post: false
-  { username: 'common_disease', start: '26 oct 2015', first: '658576374689636352', post: false },
+  { username: 'common_disease', start: '26 oct 2015', first: '658576374689636352' },
   { username: 'fxposter',       start: '19 oct 2015', first: '656019099215089664' },
   { username: 'kirshatrov',     start: '12 oct 2015', first: '653526849830678528' },
   { username: 'ptico',          start: '05 oct 2015', first: '650968820471975936' },
