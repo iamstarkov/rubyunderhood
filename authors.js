3bfcc638b523f077a5788807a086e47789fc671c
Anton Davydov <don.mist@gmail.com>
2015-12-06 21:47:29 +0300

    compile @fat0troll tweets

--- authors.js
+++ authors.js
@@ -1,6 +1,6 @@
 export default [
   // post: false
-  { username: 'fat0troll',      start: '30 nov 2015', first: '671073239632912384', post: false },
+  { username: 'fat0troll',      start: '30 nov 2015', first: '671073239632912384' },
   { username: 'pismenny',       start: '23 nov 2015', first: '668704604868186112' },
   { username: 'crashcube',      start: '16 nov 2015', first: '666150881679773696' },
   { username: 'dzaporozhets',   start: '09 nov 2015', first: '663667499335053312' },
